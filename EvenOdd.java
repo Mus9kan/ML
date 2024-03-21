4bf55fea921b4f68c15b4a551c3f23581efe5ce1
Muskan Rohe <143020974+Mus9kan@users.noreply.github.com>
2024-03-21 12:37:09 +0530

    code

---
+++ EvenOdd.java
@@ -0,0 +1,16 @@
+class EvenOdd
+{
+	public static void main(String [] args)
+	{
+	Integer s1;
+	s1 = new Integer(4);
+
+	if(s1%2==0){
+	System.out.println("the no. u have written is an even");
+	} 
+	else{
+        System.out.println("the is an odd");
+
+	}
+	}	
+}
